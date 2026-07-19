497cb3c9ad8f86c9b000dafd6e57af4f677c6ff6
Drew Hund <languageinvestigator@gmail.com>
2026-07-19 12:46:53 -0400

    init and db connection

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from app.extensions import engine, Base
+
+load_dotenv()
+
+def create_app():
+    app = FastAPI()
+
+    Base.metadata.create_all(bind=engine)
+
+    return app
