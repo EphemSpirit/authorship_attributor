6cb9fb26145e1e834e270643185b796aed2b182d
Drew Hund <languageinvestigator@gmail.com>
2026-07-20 12:20:44 -0400

    add authors table resource

---
+++ app/models/author.py
@@ -0,0 +1,9 @@
+from app.extensions import Base
+from sqlalchemy import String, JSON, Integer
+from sqlalchemy.orm import Mapped, mapped_column
+
+class Author(Base):
+    __tablename__ = "authors"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(String(50))
+    author_metadata: Mapped[dict] = mapped_column(JSON)
