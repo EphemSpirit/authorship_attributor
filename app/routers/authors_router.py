a30d67f7b631ec67f8b59df7723624d60124df98
Drew Hund <languageinvestigator@gmail.com>
2026-07-29 12:43:35 -0400

    update author response schema

--- app/routers/authors_router.py
+++ app/routers/authors_router.py
@@ -17,7 +17,11 @@ router = APIRouter(
 def get_authors(db: Annotated[Session, Depends(get_db)]):
     return db.query(Author).all()
 
-
+"""
+Main way to create an author is via document upload. This route exists for
+if you want to register an author in the database, but don't yet have any documents to feed
+the documents_router/upload endpoint
+"""
 @router.post("", response_model=AuthorRequest, status_code=status.HTTP_201_CREATED)
 def create_author(author_request: AuthorRequest, db: Annotated[Session, Depends(get_db)]):
     author = Author(name=author_request.name, author_metadata=author_request.author_metadata)
@@ -29,7 +33,12 @@ def create_author(author_request: AuthorRequest, db: Annotated[Session, Depends(
 
 @router.get("/{author_id}", response_model=AuthorRequest, status_code=status.HTTP_200_OK)
 def get_author(author_id: Annotated[int, Path()], db: Annotated[Session, Depends(get_db)]):
-    author = db.get(Author, author_id)
+    author = (
+        db.query(Author)
+        .options(joinedload(Author.style_profiles).joinedload(AuthorStyleProfile.features))
+        .filter(Author.id == author_id)
+        .first()
+    )
     if author is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Author not found")
     return author
